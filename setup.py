7c0a51738bca8041362dd679478638c8ba828983
kkkkahlua <kahluawu@gmail.com>
2020-12-21 11:44:19 -0800

    clean and update

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='shapley',
     version='0.1.0',
-    packages=['shapley', 'shapley.models', 'shapley.utils', 'shapley.use_case', 'shapley.embedding'],
+    packages=[
+        'shapley', 
+        'shapley.apps', 
+        'shapley.embedding',
+        'shapley.loader',
+        'shapley.measures',
+        'shapley.models', 
+        'shapley.utils', 
+    ],
 )
