c81e81b8a9625fcaa082a9817109a3eefd0c0825
kkkkahlua <kahluawu@gmail.com>
2020-12-26 08:41:40 -0800

    add PyTorch implementation for KNN-Shapley

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-## Shapley-Study
+# Shapley-Study
 
-### Setup
+## Setup
 
 Use the following command to install the package:
 
-```
+```bash
 python setup.py install
 ```
 
-### Overview
+## Overview
 
 We provide 13 datasets, 5 feature extractors, 5 applications, and 6 data valuation measures for a comprehensive evaluation. The choices supported are listed below. 
 
@@ -21,7 +21,7 @@ Measures        KNN-Shapley, TMC-Shapley, G-Shapley, LOO, KNN-LOO, Random
 
 The customized datasets including injected watermarks as well as other preprocessed datasets used in the code can be found on [Google Drive](https://drive.google.com/drive/folders/1vJ4PDyLq9Ud1d5NBeQjZiJHEeWqRXhrk?usp=sharing). You are recommended to download the folder ``Shapley_data`` and put it under the root folder (the same as samples.ipynb) for the purpose of testing.
 
-### Usage
+## Usage
 
 **Step 1.** Apply a certain extractor to a certain dataset to extract the embeddings, implemented in the form of ``extract_embeddings(extractor, dataset)``, e.g.
 
@@ -38,3 +38,11 @@ app.run(measure)
 ```
 
 See ``samples.ipynb`` for the sample testcases.
+
+## Changelog
+
+**2020.12.27**
+
+Add the PyTorch implementation for KNN-Shapley calculation in [shapley/measures/KNN_Shapley.py](https://github.com/AI-secure/Shapley-Study/blob/master/shapley/measures/KNN_Shapley.py). The PyTorch implementation runs faster than the original NumPy implementation since the operations are paralleled.
+
+One standalone experiment can be found in [samples.ipynb](https://github.com/AI-secure/Shapley-Study/blob/master/samples.ipynb).
--- shapley/measures/KNN_Shapley.py
+++ shapley/measures/KNN_Shapley.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.neural_network._base import ACTIVATIONS
 
+import torch
+import torch.nn.functional as F
+
 from shapley.measures import Measure
 
 class KNN_Shapley(Measure):
@@ -9,10 +12,43 @@ class KNN_Shapley(Measure):
         self.name = 'KNN_Shapley'
         self.K = K
 
-    def score(self, X_train, y_train, X_test, y_test, model_family='', model=None):
-        N = X_train.shape[0]
-        M = X_test.shape[0]
 
+    def _get_shapley_value_torch(self, X_train, y_train, X_test, y_test):
+        N = len(X_train)
+        M = len(X_test)
+
+        dist = torch.cdist(X_train.view(len(X_train), -1), X_test.view(len(X_test), -1))
+        _, indices = torch.sort(dist, axis=0)
+        y_sorted = y_train[indices]
+
+        score = torch.zeros_like(dist)
+
+        score[indices[N-1], range(M)] = (y_sorted[N-1] == y_test).float() / N
+        for i in range(N-2, -1, -1):
+            score[indices[i], range(M)] = score[indices[i+1], range(M)] + \
+                                        1/self.K * ((y_sorted[i] == y_test).float() - (y_sorted[i+1] == y_test).float()) * min(self.K, i+1) / (i+1)
+        return score.mean(axis=1)
+
+
+    def _get_shapley_value_np(self, X_train, y_train, X_test, y_test):
+        N = len(X_train)
+        M = len(X_test)
+        s = np.zeros((N, M))
+
+        for i, (X, y) in enumerate(zip(X_test, y_test)):
+            diff = (X_train - X).reshape(N, -1)
+            dist = np.einsum('ij, ij->i', diff, diff)
+            idx = np.argsort(dist)
+            ans = y_train[idx]
+            s[idx[N - 1]][i] = float(ans[N - 1] == y) / N
+            cur = N - 2
+            for j in range(N - 1):
+                s[idx[cur]][i] = s[idx[cur + 1]][i] + float(int(ans[cur] == y) - int(ans[cur + 1] == y)) / self.K * (min(cur, self.K - 1) + 1) / (cur + 1)
+                cur -= 1 
+        return np.mean(s, axis=1)
+
+
+    def score(self, X_train, y_train, X_test, y_test, model_family='', model=None):
         self.model = model
 
         if model_family == "ResNet":
@@ -23,60 +59,13 @@ class KNN_Shapley(Measure):
             X_test_out2 = resnet.layer2(X_test_out1) # 64, 32, 32
             X_out3 = resnet.layer3(X_out2)
             X_test_out3 = resnet.layer3(X_test_out2) # 64, 32, 32
-            s = np.zeros((N, M))
-            for i in range(M):
-                X = X_test_out1[i]
-                y = y_test[i]
-                dist = []
-                diff = (X_out1.detach().numpy() - X.detach().numpy()).reshape(N, -1)
-                dist = np.einsum('ij, ij->i', diff, diff)
-                idx = np.argsort(dist)
-                ans = y_train[idx]
-                s[idx[N - 1]][i] = float(ans[N - 1] == y) / N
-                cur = N - 2
-                for j in range(N - 1):
-                    s[idx[cur]][i] = s[idx[cur + 1]][i] + float(int(ans[cur] == y) - int(ans[cur + 1] == y)) / self.K * (min(cur, self.K - 1) + 1) / (cur + 1)
-                    cur -= 1
-            return np.mean(s, axis=1)
+
+            return self._get_shapley_value_np(X_train=X_out1, y_train=y_train, X_test=X_test_out1, y_test=y_test)
 
         if model_family == "NN":
             nn = model
             X_feature = ACTIVATIONS['relu'](np.matmul(X_train, nn.coefs_[0]) + nn.intercepts_[0])
             X_test_feature = ACTIVATIONS['relu'](np.matmul(X_test, nn.coefs_[0]) + nn.intercepts_[0])
-            s = np.zeros((N, M))
-            for i in range(M):
-                X = X_test_feature[i]
-                y = y_test[i]
-                dist = []
-                diff = (X_feature - X).reshape(N, -1)
-                dist = np.einsum('ij, ij->i', diff, diff)
-                idx = np.argsort(dist)
-                ans = y_train[idx]
-                s[idx[N - 1]][i] = float(ans[N - 1] == y) / N
-                cur = N - 2
-                for j in range(N - 1):
-                    s[idx[cur]][i] = s[idx[cur + 1]][i] + float(int(ans[cur] == y) - int(ans[cur + 1] == y)) / self.K * (min(cur, self.K - 1) + 1) / (cur + 1)
-                    cur -= 1  
-            return np.mean(s, axis=1)
+            return self._get_shapley_value_np(X_train=X_feature, y_train=y_train, X_test=X_test_feature, y_test=y_test)
 
-        value = np.zeros(N)
-        s = np.zeros((N, M))
-        for i in range(M):
-            X = X_test[i]
-            y = y_test[i]
-
-            dist = []
-            diff = (X_train - X).reshape(N, -1)
-            dist = np.einsum('ij, ij->i', diff, diff)
-            idx = np.argsort(dist)
-
-            ans = y_train[idx]
-
-            s[idx[N - 1]][i] = float(ans[N - 1] == y) / N
-
-            cur = N - 2
-            for j in range(N - 1):
-                s[idx[cur]][i] = s[idx[cur + 1]][i] + float(int(ans[cur] == y) - int(ans[cur + 1] == y)) / self.K * (min(cur, self.K - 1) + 1) / (cur + 1)
-                cur -= 1 
-
-            return np.mean(s, axis=1)
+        return self._get_shapley_value_np(X_train, y_train, X_test, y_test)
